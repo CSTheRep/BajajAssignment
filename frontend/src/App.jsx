3f004e3e43acac3bccf9f3d261094cd75296236f
CSTheRep <oyepkofficial@gmail.com>
2025-02-22 17:03:06 +0530

    added frontend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,8 +44,13 @@ function App() {
     };
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">22BCS15037</h1> 
+        <div className="p-10 grid content-center">
+            <h1  className="text-3xl text-center font-bold mb-4 transform transition hover:-translate-y-2 ease-in duration-700">ABCD123</h1>
+            <p className='flex space-x-2'>
+              <h2 className='text-xs text-gray-700 pb-2'>Enter your Json's Example:{'{ "data": ["A","C","z"] }'}
+              </h2>
+              <p className='pb-2 text-md animate-bounce'>👇</p>
+            </p>
             <div className="mb-4">
                 <textarea
                     className="w-full p-2 border rounded"
@@ -55,14 +60,14 @@ function App() {
                 />
             </div>
             <button
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 hover"
                 onClick={handleSubmit}
             >
                 Submit
             </button>
             {error && <p className="text-red-500 mt-2">{error}</p>}
             {response && (
-                <div className="mt-4">
+                <div className="mt-4 border p-2 rounded">
                     <div className="mb-4">
                         <label className="mr-2">Multi Filter:</label>
                         {['Numbers', 'Alphabets', 'Highest Alphabet'].map(filter => (
